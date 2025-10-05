0b53fc5b1a1542a42534b3432d9db0fdb0546605
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-10-05 23:52:00 +0700

    Add delete

--- app/Filament/Resources/DomainResource.php
+++ app/Filament/Resources/DomainResource.php
@@ -49,6 +49,7 @@ class DomainResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
