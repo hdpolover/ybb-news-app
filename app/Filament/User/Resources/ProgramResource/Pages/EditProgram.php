5b76dabfebe948f5a861930e2fb344907e565c4c
HendraMIT <suhendra@monarchintiteknologi.com>
2025-11-06 14:56:19 +0700

    fix: Correct namespace in EditProgram.php

--- app/Filament/User/Resources/ProgramResource/Pages/EditProgram.php
+++ app/Filament/User/Resources/ProgramResource/Pages/EditProgram.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\User\Resources/ProgramResource\Pages;
+namespace App\Filament\User\Resources\ProgramResource\Pages;
 
 use App\Filament\User\Resources\ProgramResource;
 use Filament\Actions;
