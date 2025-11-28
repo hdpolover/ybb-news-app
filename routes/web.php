8ee187ade3a5481b006a1387d722cc4067e44997
nandha <nandha.nandha131102@gmail.com>
2025-11-28 17:05:12 +0700

    [feat] : add API Term

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Frontend\PostController;
 use App\Http\Controllers\Frontend\CategoryController;
 
 // Frontend Routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/post/{slug}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.show');
 Route::get('/tag/{slug}', [CategoryController::class, 'show'])->name('tags.show');
 
+Route::get('/', function () {
+    return redirect('app/login');
+});
